b894035f17b84fcaf34d6e0ba3a549a24494b735
Chinonso Obidike <chinog15@ymail.com>
2018-12-24 23:20:57 -0500

    Update Rooms.java

--- Rooms.java
+++ Rooms.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package rooms;
 import javax.swing.JOptionPane;
 /**
  *This program calculates the occupancy rate in a hotel
